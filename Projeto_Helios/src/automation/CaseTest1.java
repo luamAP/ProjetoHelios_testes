12134895101b6705d3d67303d179d8609db9be36
Luan <luamlap27.9@gmail.com>
2020-02-11 20:54:27 -0400

    comit

--- Projeto_Helios/src/automation/CaseTest1.java
+++ Projeto_Helios/src/automation/CaseTest1.java
@@ -2,15 +2,27 @@ package automation;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 class CaseTest1{
 	 
-	 public static void main (String[] args) throws MalformedURLException{
+	public String email = "luamlap27.9@gmail.com";
+	
+	public AndroidDriver driver;
+	
+	 @BeforeTest
+	 public void SetupTest() throws MalformedURLException{
 			
 			//Setando o Appium
 			AppiumDriver<MobileElement> driver;
@@ -18,14 +30,33 @@ class CaseTest1{
 			DesiredCapabilities cap = new DesiredCapabilities();
 			
 			//Iniciando o dispositivo
-			cap.setCapability("deviceName", "");
+			cap.setCapability("deviceName", "emulator-5554");
 			cap.setCapability("platformName", "Android");
 			
 			//Caminho da aplicação
-			cap.setCapability("appPackage", "");
-			cap.setCapability("appActivity", "");
+			cap.setCapability("appPackage", "com.google.android.calculator");
+			cap.setCapability("appActivity", "com.android.calculator2.Calculator");
 			
 			driver = new AppiumDriver<MobileElement>(new URL ("http://127.0.0.1:4723/wd/hub"), cap);
+			
+			System.out.println("Teste Rodando...");
 	 
+	 }
+	 
+	@Test (priority = 1)
+	 public void LogandoNoSpotify() {
+		 //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 
+		 WebDriverWait wait = new WebDriverWait(driver, 10);
+		 //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.google.android.calculator:id/digit_3")));
+		 
+		 //WebDriverWait wait = new WebDriverWait(driver,10);
+		 //wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.Button")));
+		 
+		 driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
+		 //com.hecorat.screenrecorder.free:id/btn_turn_on_overlay_permission
+	
+		 System.out.println("Loging feito com sucesso!!");
+
 	 }
 }
--- Projeto_Helios/src/automation/Parte1.java
+++ Projeto_Helios/src/automation/Parte1.java
@@ -1,7 +1,25 @@
 package automation;
 
-class Parte1 {
-	
-	;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.text.ParseException;
 
+public class Parte1 {
+    public static void main(String[] args) {
+    	Calendar c0 = Calendar.getInstance();
+        Date data0 = c0.getTime();
+        DateFormat dtHora = DateFormat.getDateTimeInstance();
+        System.out.println(dtHora.format(data0));
+        
+        LocalDateTime data = LocalDateTime.now();
+        DateTimeFormatter dataFormato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = data.format(dataFormato);
+        System.out.println(formattedDate);
+    }
 }
